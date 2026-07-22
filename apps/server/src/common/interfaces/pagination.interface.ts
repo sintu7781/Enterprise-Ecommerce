19dc3194a0fc5693edb4c42d7446d3b31449300b
Sintu Kumar <sintukr7781@gmail.com>
2026-07-22 14:54:06 +0530

    feat(pagination): add PaginationMeta and PaginatedResult interfaces for pagination support

---
+++ apps/server/src/common/interfaces/pagination.interface.ts
@@ -0,0 +1,16 @@
+export interface PaginationMeta {
+  page: number;
+  limit: number;
+
+  total: number;
+  totalPages: number;
+
+  hasNextPage: boolean;
+  hasPreviousPage: boolean;
+}
+
+export interface PaginatedResult<T> {
+  data: T[];
+
+  meta: PaginationMeta;
+}
