2a02d33bb3e3911c53a16bf65765d08f7c7fd79d
Sintu Kumar <sintukr7781@gmail.com>
2026-07-24 13:19:57 +0530

    feat(middleware): reintroduce middleware exports for error handling and validation

---
+++ apps/server/src/common/middleware/index.ts
@@ -0,0 +1,6 @@
+export * from "./error.middleware.js";
+export * from "./notFound.middleware.js";
+export * from "./requestId.middleware.js";
+export * from "./validation.middleware.js";
+export * from "./authenticate.middleware.js";
+export * from "./requirePermision.js";
